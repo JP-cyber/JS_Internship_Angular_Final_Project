f77435dad29b89d76a114e6eaaca02bbe96f7e97
Yurii <greenkey870@gmail.com>
2021-03-31 16:20:38 +0300

    US003-added-user-session-support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroSelectionPageComponent } from './hero-selection-page/hero-selection-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { NewUserPageComponent } from './new-user-page/new-user-page.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
-  {path: '', component: LoginPageComponent},
+  {path: '', component: HeroSelectionPageComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginPageComponent},
   {path: 'register', component: NewUserPageComponent}
 ];
 
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { MyValidators } from '../my.validators';
+import { User } from '../shared/interfaces';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,10 +13,22 @@ import { MyValidators } from '../my.validators';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  message: string;
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) =>{
+      if(params['loginAgain']){
+        this.message = `Your current session has expired. Please login
+        again to continue using this app!`;
+      }
+    });
+
     this.form = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
@@ -28,7 +43,15 @@ export class LoginPageComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.form.value);
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset();
+      this.router.navigate(['/']);
+    });
   }
 
 }
